01b947688cd963d52d6994e0cb327d4ca3192fd5
Juan <juan.notario20@gmail.com>
2018-02-06 09:41:36 +0100

    Añdimos el .java actualizado

---
+++ PrimeraClasePrimerP.java
@@ -0,0 +1,11 @@
+package primerPaquete;
+
+public class PrimeraClasePrimerP {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundoo");
+
+	}
+
+}
+// Este es un comentario realizado mediante el editor vi
